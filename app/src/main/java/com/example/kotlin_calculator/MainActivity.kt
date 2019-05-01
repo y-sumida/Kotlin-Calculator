699a5251b706b9649df3d9e0ac055db0939cf8b2
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-01 23:06:12 +0900

    ボタンタップ時のエラー判定を実装

--- app/src/main/java/com/example/kotlin_calculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_calculator/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.kotlin_calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculate.setOnClickListener {
+            var isValid = true
+
+            val priceText = price.text.toString()
+            if (priceText.isEmpty()) {
+                price.error = getString(R.string.price_error)
+                isValid = false
+            }
+
+            val discountText = discount.text.toString()
+            if (discountText.isEmpty()) {
+                discount.error = getString(R.string.discount_error)
+                isValid = false
+            }
+
+            if (isValid) {
+                val price = Integer.parseInt(priceText)
+                val discount = Integer.parseInt(discountText)
+            }
+        }
     }
 }
