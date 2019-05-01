3c37ba248e7b6ddd7767faabc7deed0fdc371d5d
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-01 23:26:13 +0900

    計算結果を画面に表示する

--- app/src/main/java/com/example/kotlin_calculator/ResultActivity.kt
+++ app/src/main/java/com/example/kotlin_calculator/ResultActivity.kt
@@ -2,11 +2,21 @@ package com.example.kotlin_calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val price = intent.getIntExtra("price", 0)
+        val discount = intent.getIntExtra("discount", 0)
+
+        expression_label.text = getString(R.string.expression, price, discount)
+
+        val discountedPrice = price * (100 - discount) / 100
+
+        result_label.text = getString(R.string.result, discountedPrice)
     }
 }
